55ed44dc180793c57a69278cb117c13defcad9d2
Luciano Souza <lucianosoaresdesouza@gmail.com>
2023-05-29 14:01:40 -0300

    Projeto Novo Limpo com componente Exemplo

---
+++ src/components/ComponenteExemplo.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+// Para quem está usando o snack e não tem aquela ferramenta
+//do vscode que ajuda criar componente vazio
+//Pode copiar e colar esse aqui e mudar o nome
+export default ComponenteExemplo = () => {
+  return (
+    <View>
+      <Text>ComponenteExemplo</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
